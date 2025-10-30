cbfb39955c7204c218dcc7d0c647a867a5ad3f5a
NamTruong <Sirpotato070605@gmail.com>
2025-10-30 08:17:35 +0700

    Test

--- BlueLune/src/App.tsx
+++ BlueLune/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '../src/assets/images/react.svg'
+import viteLogo from '../src/assets/images/vite.svg'
+import '../src/assets/styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,13 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
